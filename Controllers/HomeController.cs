3e8b971851a54797f212448da3f17504267f24a3
alexander Abanto <alexanderabanto19@hotmail.com>
2022-03-27 14:33:05 -0500

    primera version

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using appdemo.Models;
+
+namespace appdemo.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Opcion1(Persona p)
+    {
+        Opciones(p,"Op1");
+        ViewData["Message"]="Hola estos son mis datos";
+        return View("Index",p);
+    }
+
+    public IActionResult Opcion2(Persona p)
+    {
+        Opciones(p,"Op2");
+        ViewData["Message"]="Hola estos son mis datos";
+        return View("Index",p);
+    }
+
+    private void Opciones(Persona p, String opcion){
+    if(opcion=="Op1"){
+         p.Nombre="Juan";
+         p.Apellido="Perez";
+         p.Genero="Maculino";
+         p.fechaVencimiento=new DateTime();
+        }else{
+         p.Nombre="Juana";
+         p.Apellido="Perea";
+         p.Genero="Femenino";
+         p.fechaVencimiento=new DateTime();
+        }
+
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
